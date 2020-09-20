dc2da5285f29aad80b24b20d7247a0e35608f8b1
Ankesh Kedarnath <ankesh.kedarnath@hach.com>
2020-09-21 00:46:09 +0530

    module dependency

--- README.md
+++ README.md
@@ -29,10 +29,22 @@ module "az_resource_group" {
   team_tag = "DevOps"
   creator  = "ankesh"
 }
+```
 
-output "az_rg_id" {
-  value = module.az_resource_group.az_rg_id
-}
+#### Terraform Execution:
+###### To initialize Terraform:
+```sh
+terraform init
+```
+
+###### To execute Terraform Plan:
+```sh
+terraform plan
+```
+
+###### To apply Terraform changes:
+```sh
+terraform apply
 ```
 
 <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons Licence" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.
--- variables.tf
+++ variables.tf
@@ -1,15 +1,12 @@
-variable "env" {
+variable "name" {
   description = "(Required) name of the resource group"
   default = "cloud"
 }
 
-# List of supported Azure Locations: "westus,westus2,eastus,centralus,centraluseuap,
-# | southcentralus,northcentralus,westcentralus,eastus2,eastus2euap,brazilsouth,brazilus,
-# | northeurope,westeurope,eastasia,southeastasia,japanwest,japaneast,koreacentral,koreasouth,
-# | indiasouth,indiawest,indiacentral,australiaeast,australiasoutheast,canadacentral,
-# | canadaeast,uksouth,ukwest,francecentral,francesouth,australiacentral,australiacentral2,
-# | uaecentral,uaenorth,southafricanorth,southafricawest,switzerlandnorth,switzerlandwest,germanynorth,
-# | germanywestcentral,norwayeast,norwaywest,brazilsoutheast"
+variable "env" {
+  description = "(Required) name of the resource group"
+  default = "dev"
+}
 
 variable "location" {
   description = "(Required) location where this resource has to be created"
