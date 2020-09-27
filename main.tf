307a967b77b247a9337da3fbc88f36081618a918
Ankesh Kedarnath <ankesh.kedarnath@hach.com>
2020-09-27 17:35:01 +0530

    formatting code

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ resource "azurerm_resource_group" "az_rg" {
   location = var.location
 
   tags = {
-    Region      = "var.location"
-    Team        = "var.team_tag"
-    Environment = "var.env"
-    Creator     = "var.creator"
+    Region      = var.location
+    Team        = var.team_tag
+    Environment = var.env
+    Creator     = var.creator
   }
 }
