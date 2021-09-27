9d5e5f0bb33e6c6384172069cb9156bf4add4023
Amol <50101266+amchau@users.noreply.github.com>
2021-09-27 16:52:11 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,8 +10,20 @@ resource "azurerm_resource_group" "az_rg" {
   }
 }
 
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
 
 provider "azurerm" {
-  version = "=2.20.0"
+  version = "=2.46.0"
+  client_id = var.client_id
+  subscription_id = var.subscription_id
+  client_secret = var.client_secret
+  tenant_id = var.tenant_id
   features {}
 }
