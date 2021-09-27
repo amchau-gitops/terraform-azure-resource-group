88f70e1a2a8fb573d2b3b53cc075886444e2bc16
Amol <50101266+amchau@users.noreply.github.com>
2021-09-27 18:26:59 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -22,3 +22,19 @@ variable "creator" {
   description = "(Optional) tag a creator"
   default =   "iankesh"
 }
+
+variable "client_id" {
+  description = "client_id"
+}
+
+variable "client_secret" {
+  description = "client_secret"
+}
+
+variable "subscription_id" {
+  description = "subscription_id"
+}
+
+variable "tenant_id" {
+  description = "tenant_id"
+}
