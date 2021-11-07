59b4cc2b57a615908d5d3d47fe4acde54003c078
Amol Chauhan <amol.chauhan@hotmail.com>
2021-11-07 18:45:41 +0000

    update

--- main.tf
+++ main.tf
@@ -26,9 +26,5 @@ data "external" "convert_cert" {
 #}
 
 provider "azurerm" {
-  client_id               = var.client_id
-  subscription_id         = var.subscription_id
-  client_certificate_path = "/tmp/mycert.pfx"
-  tenant_id               = var.tenant_id
   features {}
 }
