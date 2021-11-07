e80f901dba9844fab3d12799b7f60146ff8f39ba
Amol Chauhan <amol.chauhan@hotmail.com>
2021-11-07 18:51:38 +0000

    update

--- variables.tf
+++ variables.tf
@@ -1,29 +1,29 @@
 variable "name" {
   description = "(Required) name of the resource group"
-  default = "cloud"
+  default     = "cloud"
 }
 
 variable "env" {
   description = "(Required) name of the resource group"
-  default = "dev"
+  default     = "dev"
 }
 
 variable "location" {
   description = "(Required) location where this resource has to be created"
-  default = "westeurope"
+  default     = "westeurope"
 }
 
 variable "team_tag" {
   description = "(Optional) tag a team"
-  default = "DevOps"
+  default     = "DevOps"
 }
 
 variable "creator" {
   description = "(Optional) tag a creator"
-  default =   "iankesh"
+  default     = "iankesh"
 }
 
-variable "client_id" {
+variable "ARM_CLIENT_ID" {
   description = "client_id"
 }
 
@@ -31,10 +31,13 @@ variable "client_cert" {
   description = "client_certificate in base64"
 }
 
-variable "subscription_id" {
+variable "ARM_SUBSCRIPTION_ID" {
   description = "subscription_id"
 }
 
-variable "tenant_id" {
+variable "ARM_TENANT_ID" {
+  description = "tenant_id"
+}
+variable "ARM_CLIENT_CERTIFICATE_PATH" {
   description = "tenant_id"
 }
